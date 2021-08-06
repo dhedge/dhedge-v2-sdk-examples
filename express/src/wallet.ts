99fc132c59ccaf8d34564f55e681b921128a1309
hochbrj <jhochbrueckner@gmail.com>
2021-08-06 14:12:48 +1000

    use .env for secret config

--- express/src/wallet.ts
+++ express/src/wallet.ts
@@ -1,8 +1,12 @@
 import { ethers } from "@dhedge/v2-sdk"
-import { infuraProjectId, privateKey } from "./secrets"
+
+require("dotenv").config()
 
 const provider = new ethers.providers.JsonRpcProvider(
-  `https://polygon-mainnet.infura.io/v3/${infuraProjectId}`
+  `https://polygon-mainnet.infura.io/v3/${process.env.INFURA_PROJECT_ID}`
 )
 
-export const wallet = new ethers.Wallet(privateKey, provider)
+export const wallet = new ethers.Wallet(
+  process.env.PRIVATE_KEY as string,
+  provider
+)
